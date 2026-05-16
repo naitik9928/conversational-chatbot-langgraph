51b297c83ecdd71d3063b842e1df1d9a93018eb0
Naitik <naitikkumawat5100@gmail.com>
2026-05-16 09:53:42 +0530

    Switch to SQLite for chat state management
    Refactor chatbot backend to use SQLite for state management and update chat node functionality.

--- streamlit_chatbot_backend.py
+++ streamlit_chatbot_backend.py
@@ -1,30 +1,31 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
-from langgraph.graph import StateGraph,START,END
-from typing import TypedDict,Annotated
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import START,END,StateGraph
+from typing import Annotated,TypedDict
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from langchain_core.messages import SystemMessage
+import sqlite3
 load_dotenv()
-checkpointer=InMemorySaver()
+conn=sqlite3.connect(database="chatbot_backend.db",check_same_thread=False)
+checkpointer=SqliteSaver(conn=conn)
 model = ChatGroq(model="llama-3.3-70b-versatile")
-
 class ChatState(TypedDict):
     messages:Annotated[list[str],add_messages]
 
 graph=StateGraph(ChatState)
-
-
-
 def chat_node(state:ChatState):
-    system = SystemMessage(content="You are a helpful AI assistant. Be concise and clear.")
-    messages = [system] + state["messages"]
-    result = model.invoke(messages)
-    return {"messages":[result]}
-    
-
-graph.add_node("Chat",chat_node)
-graph.add_edge(START,"Chat")
-graph.add_edge("Chat",END)
-
+    system=SystemMessage(content="You are a helpful ai assistant answer carefully and precise to the cotent!")
+    message=[system]+state["messages"]
+    response=model.invoke(message)
+    return {"messages":[response]}
+graph.add_node("chat_node",chat_node)
+graph.add_edge(START,"chat_node")
+graph.add_edge("chat_node",END)
 ChatBot=graph.compile(checkpointer=checkpointer)
+
+def retriver():
+    store=set()
+    for check in checkpointer.list(None):
+        store.add(check.config["configurable"]["thread_id"])
+    return store
